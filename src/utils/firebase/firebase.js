add2fc2a71b41f9e72cde4518706d8e0ad524a03
Pieter Deane <deanepieter@gmail.com>
2023-08-06 06:28:53 +0200

    added utils

---
+++ src/utils/firebase/firebase.js
@@ -0,0 +1,50 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBFglHbzF3N-mTh-tENrejL_IwzABVQdB8',
+  authDomain: 'elegantwear-clothing-db.firebaseapp.com',
+  projectId: 'elegantwear-clothing-db',
+  storageBucket: 'elegantwear-clothing-db.appspot.com',
+  messagingSenderId: '705134857522',
+  appId: '1:705134857522:web:d2a99de55f9e1fcaa85b2b',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+    return userDocRef;
+  }
+};
