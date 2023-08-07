08cd0229c2476a048dddfb3e4a68f7b6baa07942
Pieter Deane <deanepieter@gmail.com>
2023-08-07 12:30:55 +0200

    changed the font to make it more modern , implemented signInWithGoogleRedirect

--- src/pages/sign-in/sign-in.jsx
+++ src/pages/sign-in/sign-in.jsx
@@ -1,23 +1,39 @@
- 
-import {signInWithGooglePopup , createUserDocumentFromAuth} from  '../../utils/firebase/firebase'
-
-
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+import {
+  auth,
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInWithGoogleRedirect,
+} from '../../utils/firebase/firebase';
 
 const SignIn = () => {
-  const logGoogleUser = async () => {
-    const {user} = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user)
-  }
-    return (
-    <div>
-      <h1>
-      SignIn page
-      </h1>
-      <button onClick={logGoogleUser}>signin with google</button>
-      </div>
-  )
+  useEffect(async()  => {
+    const response = await getRedirectResult(auth);
+    
+    if (response) {
+      const userDocRef = await createUserDocumentFromAuth(response.user);
+    }
+    
+    },[]);
 
-}
+
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
+
+ 
+
+  return (
+    <div>
+      <h1>SignIn page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google</button>
+      <button onClick={signInWithGoogleRedirect}>Sign in with Google redirect</button>
+    </div>
+  );
+  
+};
 
 
 export default SignIn;
