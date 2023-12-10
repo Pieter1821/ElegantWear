18d246651a8debd6e511f765c4bdcb6d07118aac
Pieter Deane <deanepieter@gmail.com>
2023-12-10 07:41:36 +0200

    adding redux thunk updated category reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import thunk from 'redux-thunk';
 
 import logger from 'redux-logger';
 
@@ -14,7 +15,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+const middlewares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean);
 
 const composeEnhancer =
   (process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
