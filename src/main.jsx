fbf38dfd01d0b3fdaa8111f8246755783e9daebf
Pieter Deane <deanepieter@gmail.com>
2023-12-27 20:40:07 +0200

    added enviroment variable

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,14 @@ import { Provider } from 'react-redux';
 import { store, persistor } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Elements } from '@stripe/react-stripe-js';
+import { stripePromise } from './utils/stripe.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <Elements>
+          <Elements stripe = {stripePromise}>
             <App />
           </Elements>
         </BrowserRouter>
