a3e53bb3c0932d6d4dbcd64530199cc6e7475102
Pieter Deane <deanepieter@gmail.com>
2023-12-27 20:33:53 +0200

    adding configuration for stripe implementation

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,16 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store, persistor } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
