7b48cd1bc974dc168c577f5b8c5e7cc5db82223c
Pieter Deane <deanepieter@gmail.com>
2023-12-29 21:09:01 +0200

    added serverless function create payment intent on server also added the onScroll handler that can be toggled when paymentHandler is called submit button

--- src/components/payment-form/payment-form.jsx
+++ src/components/payment-form/payment-form.jsx
@@ -11,12 +11,23 @@ export const PaymentForm = () => {
     e.preventDefault();
 
     if (!stripe || !elements) {
+      console.error('Stripe or Elements not initialized');
       return;
     }
+
+    const res = await fetch('/.netlify/functions/create-payment-intent', {
+      method: 'post',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({ amount: 10000 }),
+    }).then((res) => res.json());
+      console.log(response);
   };
+
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onScroll={paymentHandler} >
         <h2>Credit Card Payment:</h2>
         <CardElement />
         <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
