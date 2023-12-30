e3e0c772598503a3b70d82cb7a0c097fe695db55
Pieter Deane <deanepieter@gmail.com>
2023-12-30 06:04:51 +0200

    added error handling

--- src/components/payment-form/payment-form.jsx
+++ src/components/payment-form/payment-form.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 import { PaymentFormContainer, FormContainer } from './payment-form.styles';
 
@@ -15,22 +16,50 @@ export const PaymentForm = () => {
       return;
     }
 
-    const res = await fetch('/.netlify/functions/create-payment-intent', {
-      method: 'post',
-      headers: {
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify({ amount: 10000 }),
-    }).then((res) => res.json());
-      console.log(response);
+    try {
+      // Call  serverless function to create a payment intent
+      const response = await fetch('/.netlify/functions/create-payment-intent', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ amount: 10000 }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to create payment intent');
+      }
+
+      const paymentIntent = await response.json();
+
+      // Confirm the payment on the client side
+      const result = await stripe.confirmCardPayment(paymentIntent.client_secret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+        },
+      });
+
+      if (result.error) {
+        console.error(result.error.message);
+        // Handle payment error
+      } else if (result.paymentIntent.status === 'succeeded') {
+        console.log('Payment succeeded');
+        // Handle payment success
+      }
+    } catch (error) {
+      console.error('Error:', error.message);
+      
+    }
   };
 
   return (
     <PaymentFormContainer>
-      <FormContainer onScroll={paymentHandler} >
+      <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment:</h2>
         <CardElement />
-        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
+        <Button buttonType={BUTTON_TYPE_CLASSES.inverted} type="submit">
+          Pay Now
+        </Button>
       </FormContainer>
     </PaymentFormContainer>
   );
