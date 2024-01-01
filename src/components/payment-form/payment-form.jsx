73ce2e8425da889422d6a42406cf1f5e80790c98
Pieter Deane <deanepieter@gmail.com>
2024-01-01 18:27:13 +0200

    added improvements

--- src/components/payment-form/payment-form.jsx
+++ src/components/payment-form/payment-form.jsx
@@ -1,55 +1,45 @@
-import React from 'react';
-import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
-import { PaymentFormContainer, FormContainer } from './payment-form.styles';
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
-import Button, { BUTTON_TYPE_CLASSES } from '../button/button';
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button";
+
+import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
 
 export const PaymentForm = () => {
-  const stripe = useStripe();
-  const elements = useElements();
+	const stripe = useStripe();
+	const elements = useElements();
 
-  const paymentHandler = async (e) => {
-    e.preventDefault();
+	const paymentHandler = async (e) => {
+		e.preventDefault();
 
-    if (!stripe || !elements) {
-      console.error('Stripe or Elements not available.');
-      return;
-    }
+		if (!stripe || !elements) {
+			return;
+		}
 
-    try {
-      const paymentIntentEndpoint = '/.netlify/functions/create-payment-intent';
+		//netlify function (first serverless function)
+		const response = await fetch(
+			"/.netlify/functions/create-payment-intent",
+			{
+				method: "post",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ amount: 10000 }),
+			}
+		).then((res) => res.json());
 
-      const response = await fetch(paymentIntentEndpoint, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ amount: 10000 }),
-      });
+		console.log(response);
+	};
 
-      if (!response.ok) {
-        throw new Error(error);
-      }
-
-      const responseData = await response.json();
-      console.log('Response data:', responseData);
-    } catch (error) {
-      console.error('Error creating payment intent:', error);
-    }
-  };
-
-  return (
-    <PaymentFormContainer>
-      <FormContainer onSubmit={paymentHandler}>
-        <h2>Credit Card Payment:</h2>
-        <label>
-          Card Details
-          <CardElement />
-        </label>
-        <Button type="submit" buttonType={BUTTON_TYPE_CLASSES.inverted}>
-          Pay Now
-        </Button>
-      </FormContainer>
-    </PaymentFormContainer>
-  );
+	return (
+		<PaymentFormContainer>
+			<FormContainer onSubmit={paymentHandler}>
+				<h2>Credit Card Payment: </h2>
+				<CardElement />
+				<Button buttonType={BUTTON_TYPE_CLASSES.inverted}>
+					Pay now
+				</Button>
+			</FormContainer>
+		</PaymentFormContainer>
+	);
 };
+export default PaymentForm;
