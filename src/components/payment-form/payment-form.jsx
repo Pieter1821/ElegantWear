d5d25232eb0e2cad96f091bb634d4db084837d25
Pieter Deane <deanepieter@gmail.com>
2024-01-02 06:38:14 +0200

    Implement payment confirmation logic

--- src/components/payment-form/payment-form.jsx
+++ src/components/payment-form/payment-form.jsx
@@ -1,45 +1,60 @@
-import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
-
-import Button, { BUTTON_TYPE_CLASSES } from "../button/button";
-
-import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
+import React from 'react';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button';
+import { PaymentFormContainer, FormContainer } from './payment-form.styles';
 
 export const PaymentForm = () => {
-	const stripe = useStripe();
-	const elements = useElements();
+  const stripe = useStripe();
+  const elements = useElements();
 
-	const paymentHandler = async (e) => {
-		e.preventDefault();
+  const paymentHandler = async (e) => {
+    e.preventDefault();
 
-		if (!stripe || !elements) {
-			return;
-		}
+    if (!stripe || !elements) {
+      return;
+    }
 
-		//netlify function (first serverless function)
-		const response = await fetch(
-			"/.netlify/functions/create-payment-intent",
-			{
-				method: "post",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({ amount: 10000 }),
-			}
-		).then((res) => res.json());
+    const response = await fetch('/.netlify/functions/create-payment-intent', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ amount: 10000 }),
+    });
 
-		console.log(response);
-	};
+    const {
+      paymentIntent: { client_secret },
+    } = Response;
 
-	return (
-		<PaymentFormContainer>
-			<FormContainer onSubmit={paymentHandler}>
-				<h2>Credit Card Payment: </h2>
-				<CardElement />
-				<Button buttonType={BUTTON_TYPE_CLASSES.inverted}>
-					Pay now
-				</Button>
-			</FormContainer>
-		</PaymentFormContainer>
-	);
+    console.log(client_secret);
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: 'Pieter',
+        },
+      },
+    });
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === 'succeeded') {
+        alert('Payment Successful');
+      }
+    }
+  };
+
+  return (
+    <PaymentFormContainer>
+      <FormContainer onSubmit={paymentHandler}>
+        <h2>Credit Card Payment: </h2>
+        <CardElement />
+        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
+      </FormContainer>
+    </PaymentFormContainer>
+  );
 };
+
 export default PaymentForm;
