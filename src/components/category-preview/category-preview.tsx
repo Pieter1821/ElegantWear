800156206c806c81a5ffee0a7ee7efeaaa68fd62
Pieter Deane <deanepieter@gmail.com>
2024-02-02 06:25:08 +0200

    added typing for category-preview

--- src/components/category-preview/category-preview.tsx
+++ src/components/category-preview/category-preview.tsx
@@ -1,34 +1,36 @@
-import {CategoryPreviewContainer,Title,Preview} from './category-preview.styles';
+import { CategoryPreviewContainer, Title, Preview } from './category-preview.styles';
 import ProductCard from '../product-card/product-card';
+import { FC } from 'react';
+
 
 export type Product = {
-    id: string;
-    name: string;
-    price: number;
-    imageUrl: string;
-  
-} 
+  id: string;
+  name: string;
+  price: number;
+  imageUrl: string;
 
-export type CategoryPreviewProps = {
-    title: string;
-    products: ;
-
-    // TODO typing category-preview
 }
 
-const CategoryPreview = ({ title, products }) => {
+export type CategoryPreviewProps = {
+  title: string;
+  products: Product[];
+
+
+}
+
+const CategoryPreview: FC<CategoryPreviewProps> = ({ title, products }) => {
   return (
     <CategoryPreviewContainer>
       <h2>
-        <Title to={title}>{title.toUpperCase()}</Title>
+        <Title to={`/${title}`}>{title.toUpperCase()}</Title>
       </h2>
       <Preview>
         {products
           .filter((_, idx) => idx < 4)
           .map((product) => (
             <ProductCard key={product.id} product={product} />
-            ))}
-            
+          ))}
+
       </Preview>
     </CategoryPreviewContainer>
   );
