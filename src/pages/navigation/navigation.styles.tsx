2eb32abc97812393595da4bf292515d6db416462
Pieter Deane <deanepieter@gmail.com>
2024-02-19 12:15:56 +0200

    improved styling checkout and navigation bar improved responsiveness

--- src/pages/navigation/navigation.styles.tsx
+++ src/pages/navigation/navigation.styles.tsx
@@ -2,69 +2,78 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const NavigationContainer = styled.div`
-  height: 70px;
+  height: 70px; 
   width: 100%;
   display: flex;
-  flex-direction: column; 
+  flex-direction: column;
   justify-content: space-between;
-  margin-bottom: 25px;
   padding: 10px;
+  box-sizing: border-box;
 
   @media screen and (min-width: 700px) {
-    height: 60px;
+    height: 10vh;
     flex-direction: row;
-    margin-bottom: 20px;
+    justify-content: space-between; 
+    margin-bottom: 2rem;
     padding: 0;
   }
 `;
 
 export const LogoContainer = styled(Link)`
   height: 70px;
-  width: 100%;
-  padding: 15px;
+  width: 70px; // Fixed size for mobile
+  padding: 1rem;
 
   @media screen and (min-width: 300px) {
-    width: 50px;
+    width: 15%; 
     padding: 0;
+    flex-shrink: 0; 
   }
 `;
 
 export const NavLinks = styled.div`
-  width: 100%;
-  height: auto; 
   display: flex;
-  flex-direction: column; 
-  gap: 10px; 
+  flex-direction: column;
+  gap: 1rem;
+  align-items: center; 
 
   @media screen and (min-width: 358px) {
-    flex-direction: row; 
-    justify-content: center; 
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
   }
 
   @media screen and (min-width: 700px) {
-    width: auto;
     flex-direction: row;
-    justify-content: flex-end; 
+    justify-content: flex-end;
+    align-items: center;
+    flex-grow: 1;
   }
 `;
 
 export const NavLink = styled(Link)`
+  flex: 1;
   padding: 10px 15px;
   cursor: pointer;
   text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   @media screen and (min-width: 358px) {
-    padding: 10px; 
+    padding: 0.5rem 1rem;
   }
 `;
 
 export const Icon = styled.div`
-  display: none; 
+  @media screen and (max-width: 
+700px) {
+display: flex;
+justify-content: center;
+align-items: center;
+}
 
-  @media screen and (max-width: 358px) {
-    display: flex; 
-    justify-content: center;
-    align-items: center;
-    margin-bottom: 10px; 
-  }
+@media screen and (min-width: 700px) {
+display: none; 
+}
 `;
